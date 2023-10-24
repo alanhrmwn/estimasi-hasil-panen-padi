3453385a9994988b3a2696b5aed6108da57b2c52
alanhrmwn <148874522+alanhrmwn@users.noreply.github.com>
2023-10-24 19:47:47 +0700

    Add files via upload

---
+++ estimasi-hasil-panen-padi.py
@@ -0,0 +1,19 @@
+import pickle
+import streamlit as st
+
+model = pickle.load(open('estimasi_padi.sav', 'rb'))
+
+st.title('Estimasi Hasil Panen Padi di Sumatera')
+
+Luas_Panen = st.number_input('Luas Pertanian (hektar)')
+Curah_hujan = st.number_input('Jumlah rata-rata curah hujan dalam setahun (milimeter)')
+Kelembapan = st.number_input('Tingkat kelembaban rata-rata dalam setahun (persentase)')
+Suhu_rata_rata = st.number_input('Derajat suhu rata-rata dalam setahun (celsius)')
+
+predict = ''
+
+if st.button('Estimasi Nilai'):
+    predict = model.predict(
+        [[Luas_Panen, Curah_hujan, Kelembapan, Suhu_rata_rata]]
+    )
+    st.write ('Estimasi Hasil Panen Padi (ton) : ', predict)
